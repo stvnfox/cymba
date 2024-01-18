5423d9ff3a1767cd76484609e5bf18942463001d
Steven <steven@stvnx.nl>
2024-01-18 16:49:51 +0100

    style: update to login page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { LoginComponent } from '@/components/LoginComponent'
 const Home = () => {
   return (
     <main className="flex flex-col justify-center items-center min-h-screen w-screen">
-      <h1 className='text-3xl font-bold tracking-wider mb-8'>Cymba</h1>
       <LoginComponent/>
     </main>
   )
--- src/components/LoginComponent.tsx
+++ src/components/LoginComponent.tsx
@@ -7,11 +7,12 @@ import { redirectToSpotifyAuth } from '../lib/auth';
 
 export const LoginComponent: FunctionComponent = () => {
     return (
-        <div className='flex flex-col items-center bg-neutral-700 rounded-lg w-[400px] p-12'>
-            <h2 className="text-neutral-200 mb-8">
-                To enter the world of Cymba we need you to login with your Spotify account
-            </h2>
-            <button className='flex items-center gap-2 bg-green-700 hover:bg-green-800 text-neutral-200 rounded-lg transition-colors px-6 py-4' onClick={redirectToSpotifyAuth}>
+        <div className='bg-neutral-300 font-light rounded-lg shadow-2xl shadow-neutral-900 w-[400px] px-12 py-24'>
+            <h1 className="text-2xl text-green-800 tracking-wide mb-12">Cymba</h1>
+            <p className="text-green-950 tracking-wide mb-12 lg:w-5/6">
+                <b>Welcome back,</b> connect with your Spotify account to continue to Cymba.
+            </p>
+            <button className='flex items-center gap-3 bg-green-700 hover:bg-green-800 text-neutral-200 tracking-wide rounded-lg transition-colors px-6 py-4' onClick={redirectToSpotifyAuth}>
                 <FontAwesomeIcon icon={faSpotify} className='h-8'/>
                 Login with Spotify
             </button>
